2a5b34b8dff6b46cb242fc2299c3f626cb73471a
kellypark01 <hera002250@gmail.com>
2023-04-26 10:34:29 +0900

    정규표현식 연습1

--- doc_copy_readline2.py
+++ doc_copy_readline2.py
@@ -9,11 +9,11 @@ g=open('c:\\work\\PV3_copy.txt','wt',encoding='utf-8')  # 복사본
 #한번에 한줄씩 읽어서 처리한다.  
 #파일의 EOF(End Of File)이 아니면 계속 읽도록 한다. 
 line = f.readline()
-while (line != ''):
-    if (re.search("\d{4}", line)):  # 숫자 4자리가 연달아서 나오는 라인 찾아줘
+while (line != ''):                   # EOF가 아니면 파일을 읽고, EOF이면 종료함
+    if (re.search("\d{4}", line)):    # 숫자 4자리가 연달아서 나오는 라인 찾아줘
 #    if (re.search("error", line)):   # error가 있는지 찾아라
-        g.write(line + "\n")         # 검색한 라인을 복사본에 저장해서 만들어줘
-    line = f.readline()
+        g.write(line + "\n")          # 검색한 라인을 복사본에 저장해서 만들어줘
+    line = f.readline()               # 다음 줄 읽어와라
 
 f.close() 
 g.close()
