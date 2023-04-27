af1ebf4de3ed96cbeee786fb9b8c232265b98ccb
kellypark01 <hera002250@gmail.com>
2023-04-27 10:44:59 +0900

    클리앙 중고장터 검색

--- web2.py
+++ web2.py
@@ -1,22 +1,31 @@
 # web2.py 
 #웹서버와 통신 
-import requests
+import requests #웹서버에 요청할 수 있는 라이브러리 요청
 #크롤링
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  #BeautifulSoup 모듈
 
-url = "https://www.daangn.com/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+url = "https://www.daangn.com/"  # 당근마켓 문자열 변수url에 담아서
+response = requests.get(url)     # 요청 후 response 문자열로 받음
+soup = BeautifulSoup(response.text, "html.parser")   # soup이라는 객체를 보낸다
 
-posts = soup.find_all("div", attrs={"class":"card-desc"})
+posts = soup.find_all("div", attrs={"class":"card-desc"})  # find_all 전부 다 가져와 div인데 속성이 class가 card-desc 인거 
+#파일에 저장
+f = open("c:\\work\\daangn.txt", "wt", encoding="utf-8")   # open 함수를 사용해서 파일로 떨군다.
 for post in posts:
     title = post.find("h2", attrs={"class":"card-title"})
     price = post.find("div", attrs={"class":"card-price"})
-    print("{0} , {1}".format(title, price))     # 하나의 문자열로 만들어서 출력한다.
-    
+    title = title.text.replace("\n", "") # \n이 있으면 공백 제거
+    price = price.text.replace("\n", "")    
+    #print("{0} , {1}".format(title.text, price.text))     # 하나의 문자열로 만들어서 출력한다. .text (속성)를 붙인 이유는 앞뒤의 태그를 제거하고 안에 있는 컨텐츠만 보여주기 위함
+    print("{0} , {1}".format(title, price))  
+    result = f"매물: {title} 가격: {price} \n"   # 
+    f.write(result)
+
+f.close()  # for 루프 끝나고 파일 닫기
+
     #ctrl + /:선택영역 주석처리 
     # <div class="card-desc">
-    #       <h2 class="card-title">나이키 신발 팝니다!</h2>
+    #       <h2 class="card-title">나이키 신발 팝니다!</h2>   # h2 태그는 1개이다.클래스 속성이 card-title이다.
     #       <div class="card-price ">
     #         10,000원
     #       </div>
---
+++ 클리앙중고장터검색.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) - 헤더를 넣어줘서 웹봇이 아니라고 속인다.
+hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+#페이지 주소를 0부터 9까지 생성 ( 페이지 주소를 보고 파악해야함 )
+for n in range(0,10):  # 0부터 9까지의 정수를 만들어준다. 규칙이 있는 수열이므로 range 사용
+        #클리앙의 중고장터 주소 
+        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)   # &po= 페이지 번호 &po=0 는 1페이지
+        print(data)
+        #웹브라우져 헤더 추가 
+        req = urllib.request.Request(data, \
+                                    headers = hdr)
+        data = urllib.request.urlopen(req).read()   # data 분자열 변수
+        #한글이 깨지는 경우 다시 해석
+        page = data.decode('utf-8', 'ignore')  # data는 문자열 변수이고 utf-8 유니코드로 다시 해석하고 ignore 글자가 깨져도 상관없다. 
+        #2300줄 문자열 넘기는 코드
+        soup = BeautifulSoup(page, 'html.parser')
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})   # span 태그 찾기
+        # <span class="subject_fixed" data-role="list-title-text" title="아이패드 9세대 64GB 셀룰러 LTE 단순개봉 새제품급 판매합니다.">
+        # 	아이패드 9세대 64GB 셀룰러 LTE 단순개봉 새제품급 판매합니다.
+        # </span>        
+
+        for item in list:
+                try:    # 시도해보고 에러가 나면 나가fk ( try : 에러 처리하는 키워드)
+                        title = item.text.strip()  # 태그는 제외하고 공백 제거
+                        print(title)
+                        # if (re.search('아이폰', title)):
+                        #         print(title.strip())
+                except:
+                        pass
+        
