191428c8bba8a54f053a66e0e3641f2118811c51
kellypark01 <hera002250@gmail.com>
2023-04-28 10:44:20 +0900

    데이터 분석 연습1

---
+++ 둘다 선언.py
@@ -0,0 +1,10 @@
+#둘다 선언
+from pandas import Series, DataFrame
+import pandas as pd
+
+myCampus = Series( [100,200,300,400,500],
+    index=["2023-04-25", "2023-04-26", "2023-04-27", "2023-04-28","2023-04-29"])
+type(myCampus)
+
+#슬라이싱
+myCampus
